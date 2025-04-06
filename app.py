f8912323e877641cffa4782887697d29a43e3cd4
deepakpro190 <deepak.goel.pro@gmail.com>
2025-04-06 12:16:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def home():
             session.modified = True
 
     return render_template("index.html", chat_history=session["chat_history"])
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
