67d52cea542273c2bf308f5d31a32f9b3c232475
deepakpro190 <deepak.goel.pro@gmail.com>
2025-04-07 19:54:45 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="shl_test_app",
-    version="0.1",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[],
-)
