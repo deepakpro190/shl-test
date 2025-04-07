5e4dee46d571453f7cbef218ffdeeeb5016009ec
deepakpro190 <deepak.goel.pro@gmail.com>
2025-04-07 19:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def process_query():
             "response": f"Error: {str(e)}"
         })
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Fallback to 8000 locally
+    port = int(os.environ.get("PORT", 10000))  # Fallback to 8000 locally
     app.run(host="0.0.0.0", port=port)
